a44239127bdcf26bea8b530c2589521c22e763ce
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-03-26 11:31:54 -0400

    working model proxy

--- example/samplelowkie.js
+++ example/samplelowkie.js
@@ -3,16 +3,30 @@ const lowkie = require('../index');
 const lowkie2 = require('../index');
 const lowkie3 = require('../index');
 
-console.log( 'lowkie.connection', lowkie.connection );
+// console.log( 'lowkie.connection', lowkie.connection );
 // console.log(lowkie === lowkie2);
 lowkie.connect(path.join(__dirname, './sampledb.json'));
 lowkie.connection.on('connecting', (connectdata)=>{
-  console.log('now trying to connect to db',connectdata);
+  console.log('now trying to connect to db');
 });
 lowkie.connection.once('connected', (connectdata)=>{
   console.log('now connected to db');
+  var kittySchema = lowkie.Schema({
+    name: String,
+  });
+  let Kitten = lowkie.model('kitten', kittySchema);
+  Kitten.insert({
+    name: 'princess',
+  })
+    .then(newkitty => {
+      console.log({ newkitty });
+    })
+    .catch(e => { 
+      console.log(e);
+    });
+  // console.log(lowkie.db);
+  let t = setInterval(() => {
+    console.log('running process');
+  }, 5000);
 });
 
-let t = setInterval(() => {
-  console.log('running process');
-}, 5000);
